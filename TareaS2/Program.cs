9db2118a8674e91cfe51dfe4835fdf7b38b56765
Mijeli <mlucerob@miumg.edu.gt>
2024-02-16 13:51:24 -0600

    Programa No. 19

--- TareaS2/Program.cs
+++ TareaS2/Program.cs
@@ -1,15 +1,6 @@
-﻿Console.WriteLine("Ingrese el valor en X del primer punto: ");
-double x1 = Convert.ToDouble(Console.ReadLine());
+﻿Console.WriteLine("Ingrese la temperatura en grados Celsius: ");
+double celsius = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Ingrese el valor en Y del primer punto: ");
-double y1 = Convert.ToDouble(Console.ReadLine());
+double fahrenheit = (celsius * 1.8) + 32;
 
-Console.WriteLine("Ingrese el valor en X del segundo punto: ");
-double x2 = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Ingrese el valor en Y del segundo punto: ");
-double y2 = Convert.ToDouble(Console.ReadLine());
-
-double distancia = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-
-Console.WriteLine("La distancia entre los dos puntos es de: {0}", distancia);
+Console.WriteLine("La temperatura en grados Fahrenheit es de: {0}", fahrenheit);
