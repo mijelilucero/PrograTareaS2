64689dd33d1dabb098cbb5d0c95fdb8eb329a9dc
Mijeli <mlucerob@miumg.edu.gt>
2024-02-16 01:14:29 -0600

    Programa No. 10

--- TareaS2/Program.cs
+++ TareaS2/Program.cs
@@ -4,5 +4,5 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Ingrese el entero 2: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-int resultado = num1 / num2;
-Console.WriteLine("El resultado de la divisíon de {0} entre {1} es: {2}", num1, num2, resultado);
+int resultado = num1 % num2;
+Console.WriteLine("El modulo de la divisíon de {0} entre {1} es: {2}", num1, num2, resultado);
