bc05e27fa4b63dce47c9a6ac0c8b38cf8460824e
Mijeli <mlucerob@miumg.edu.gt>
2024-02-16 00:59:36 -0600

    Programa No. 6

--- TareaS2/Program.cs
+++ TareaS2/Program.cs
@@ -1,6 +1,8 @@
-﻿Console.WriteLine("Ingrese un valor entero: ");
+﻿Console.WriteLine("Ingrese el entero 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
 
-int numInt = Convert.ToInt32(Console.ReadLine());
-Char caracter = (Char)numInt;
+Console.WriteLine("Ingrese el entero 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("El numero {0} convertido a char es: '{1}'",numInt,caracter);
+int resultado = num1 + num2;
+Console.WriteLine("La suma de {0} y {1} es: {2}", num1, num2, resultado);
