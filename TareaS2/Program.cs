21a656f799bf39d7a95d8e319771fda888d98051
Mijeli <mlucerob@miumg.edu.gt>
2024-02-16 01:08:29 -0600

    Programa No. 8

--- TareaS2/Program.cs
+++ TareaS2/Program.cs
@@ -1,8 +1,5 @@
-﻿Console.WriteLine("Ingrese el decimal 1: ");
-decimal num1 = Convert.ToDecimal(Console.ReadLine());
+﻿float num1 = 12.34f;
+float num2 = 24.56f;
 
-Console.WriteLine("Ingrese el decimal 2: ");
-decimal num2 = Convert.ToDecimal(Console.ReadLine());
-
-decimal resultado = num1 - num2;
-Console.WriteLine("La resta de {0} menos {1} es: {2}", num1, num2, resultado);
+float resultado = num1 * num2;
+Console.WriteLine("La multiplicacion de {0} por {1} es: {2}", num1, num2, resultado);
