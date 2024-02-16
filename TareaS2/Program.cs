9becfa9904874ef1e9d02050799a83410fcf34d0
Mijeli <mlucerob@miumg.edu.gt>
2024-02-16 01:23:15 -0600

    Programa No. 11

--- TareaS2/Program.cs
+++ TareaS2/Program.cs
@@ -1,8 +1,5 @@
-﻿Console.WriteLine("Ingrese el entero 1: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine("Ingrese un texto corto: ");
+string cadena = Console.ReadLine();
 
-Console.WriteLine("Ingrese el entero 2: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-
-int resultado = num1 % num2;
-Console.WriteLine("El modulo de la divisíon de {0} entre {1} es: {2}", num1, num2, resultado);
+string resultado = cadena.ToUpper();
+Console.WriteLine(resultado);
