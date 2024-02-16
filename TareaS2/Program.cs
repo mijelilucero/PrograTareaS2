28a4ba55cdeae73fa4453f33edc8b2f2a9ca9ded
Mijeli <mlucerob@miumg.edu.gt>
2024-02-16 01:02:59 -0600

    Programa No. 7

--- TareaS2/Program.cs
+++ TareaS2/Program.cs
@@ -1,8 +1,8 @@
-﻿Console.WriteLine("Ingrese el entero 1: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine("Ingrese el decimal 1: ");
+decimal num1 = Convert.ToDecimal(Console.ReadLine());
 
-Console.WriteLine("Ingrese el entero 2: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese el decimal 2: ");
+decimal num2 = Convert.ToDecimal(Console.ReadLine());
 
-int resultado = num1 + num2;
-Console.WriteLine("La suma de {0} y {1} es: {2}", num1, num2, resultado);
+decimal resultado = num1 - num2;
+Console.WriteLine("La resta de {0} menos {1} es: {2}", num1, num2, resultado);
